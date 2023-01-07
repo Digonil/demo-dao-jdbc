7448a9158c157f15139be4df6b596e5057117d48
Diego Niles <diego.niles@hotmail.com>
2023-01-07 19:25:53 -0300

    Update implementation

--- src/main/java/br/com/udemy/demodaojdbc/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/br/com/udemy/demodaojdbc/model/dao/impl/SellerDaoJDBC.java
@@ -67,6 +67,31 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public void update(Seller obj) {
 
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = conn.prepareStatement("UPDATE seller\n"
+                    + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?\n"
+                    + "WHERE Id = ?");
+
+            ps.setString(1, obj.getName());
+            ps.setString(2, obj.getEmail());
+            ps.setDate(3, java.sql.Date.valueOf(obj.getBirthDate()));
+            ps.setDouble(4, obj.getBaseSalary());
+            ps.setInt(5, obj.getDepartment().getId());
+            ps.setInt(6, obj.getId());
+
+            ps.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(ps);
+        }
+
+
     }
 
     @Override
